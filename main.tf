88881a7288baaa3415abbee2bbfc34d01383e720
hari <ghariharan24@gmail.com>
2024-06-04 19:31:33 +0530

    added security grp

--- main.tf
+++ main.tf
@@ -2,6 +2,29 @@ resource "aws_instance" "public_instance" {
   ami           = var.ami
   instance_type = var.instance_type
 
+  # Inbound rules
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   user_data = <<-EOF
               #!/bin/bash
               # Install updates
@@ -9,12 +32,6 @@ resource "aws_instance" "public_instance" {
 
               # Install JDK
               sudo apt-get install openjdk-11-jdk
-
-              # Install Docker
-              amazon-linux-extras install docker -y
-              systemctl start docker
-              systemctl enable docker
-              usermod -a -G docker ec2-user
 
               # Install Node.js
               sudo apt-get install -y nodejs
@@ -24,10 +41,10 @@ resource "aws_instance" "public_instance" {
               sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \https://pkg.jenkins.io/debian/jenkins.io-2023.key
               echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \https://pkg.jenkins.io/debian binary/ | sudo tee \/etc/apt/sources.list.d/jenkins.list > /dev/null
               sudo apt-get install jenkins
-              systemctl start jenkins.service
+              sudo systemctl start jenkins.service
 
               # Additional setup to allow Jenkins to run Docker
-              sudo apt-get install docker-ce -y
+              sudo apt-get install -y docker.io
               sudo systemctl start docker
               sudo systemctl enable docker
               sudo usermod -aG docker jenkins
